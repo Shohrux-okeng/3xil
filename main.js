4906ca459adf47e148464baeec7869582c4b90df
AlamovShohrux <you@example.com>
2025-05-01 13:28:03 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,64 @@
+1; //declarition
+// let a = 2;
+// function foo(a) {
+//     return a*a*a;
+// }
+// console.log(foo(a));
+
+//const
+// const foo = function(son) {
+//     return son * son * son;
+// };
+// console.log(foo(2));
+
+//arrow
+// const foo = son => son * son * son;
+// console.log(foo(2));
+
+2
+//declarition
+// let a = prompt("yoshingizni kiriting");
+// if (a < 18) {
+//   alert("voyaga yetmagansiz xali");
+// } else {
+//   alert("yoshingiz katta");
+// }
+
+
+//expression
+// const Yosh = function(yosh) {
+//     if (yosh >= 18) {
+//         return "yoshingiz katta";
+//     } else {
+//         return "Voyaga yetmagan siz xali";
+//     }
+// };
+// console.log(Yosh(16));
+
+
+//arrow
+// const Yosh = son => son >= 18 ? "yoshingiz katta" : "voyaga yetmagan siz xali";
+// console.log(Yosh(20));
+
+
+4
+//declarition
+// function engKichik(a, b, c) {
+//     return Math.min(a, b, c);
+//   }
+//   console.log(engKichik(4, 9, 2));
+
+
+
+// expresion
+
+// const engKichik = function(a, b, c) {
+//     return Math.min(a, b, c);
+//   }
+//   console.log(engKichik(7, 1, 5));
+  
+
+
+//arrow
+// const engKichik = (a, b, c) => Math.min(a, b, c);
+// console.log(engKichik(8, 3, 6));
