bff40fd44a0da4fa834c1b668491f5a9cd13cde6
AlamovShohrux <you@example.com>
2025-05-08 12:03:26 +0500

    first commit

--- main.js
+++ main.js
@@ -1,64 +1,26 @@
-1; //declarition
-// let a = 2;
-// function foo(a) {
-//     return a*a*a;
-// }
-// console.log(foo(a));
-
-//const
-// const foo = function(son) {
-//     return son * son * son;
-// };
-// console.log(foo(2));
-
-//arrow
-// const foo = son => son * son * son;
-// console.log(foo(2));
-
-2
-//declarition
-// let a = prompt("yoshingizni kiriting");
-// if (a < 18) {
-//   alert("voyaga yetmagansiz xali");
-// } else {
-//   alert("yoshingiz katta");
-// }
-
-
-//expression
-// const Yosh = function(yosh) {
-//     if (yosh >= 18) {
-//         return "yoshingiz katta";
-//     } else {
-//         return "Voyaga yetmagan siz xali";
-//     }
-// };
-// console.log(Yosh(16));
-
-
-//arrow
-// const Yosh = son => son >= 18 ? "yoshingiz katta" : "voyaga yetmagan siz xali";
-// console.log(Yosh(20));
-
-
-4
-//declarition
-// function engKichik(a, b, c) {
-//     return Math.min(a, b, c);
+1
+// function findIndex(arr, value) {
+//     return arr.indexOf(value);
 //   }
-//   console.log(engKichik(4, 9, 2));
-
-
-
-// expresion
-
-// const engKichik = function(a, b, c) {
-//     return Math.min(a, b, c);
-//   }
-//   console.log(engKichik(7, 1, 5));
+//   let arr = [10, 7, 8, 55, 44];
+//   let value = 8;
+//   console.log(findIndex(arr, value));
   
+  2
+//   function addToEnd(arr, value) {
+//     arr.push(value);
+//     return arr;
+//   }
 
+//   let arr = [1, 2, 3];
+//   let value = 5;
+//   console.log(addToEnd(arr, value));
+  
+5
+// function okay(arr) {
+//     return arr.sort((a, b) => a - b);
+//   }
 
-//arrow
-// const engKichik = (a, b, c) => Math.min(a, b, c);
-// console.log(engKichik(8, 3, 6));
+//   let arr = [2, 1, 3];
+//   console.log(okay(arr));
+  
